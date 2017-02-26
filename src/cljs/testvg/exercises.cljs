8f7d3c0272e38ed6d6c30341877bbc5bc9527975
Oliver Mooney <oliver.mooney@gmail.com>
2017-02-26 11:50:44 +0000

    Added reagent timer example

--- src/cljs/testvg/exercises.cljs
+++ src/cljs/testvg/exercises.cljs
@@ -1,5 +1,14 @@
 (ns testvg.exercises
-  "The Vigiglobe reagent test exercises.")
+  "The Vigiglobe reagent test exercises."
+  (:require [reagent.core :as r]))
+
+(defn timer
+  "The reagent timer example."
+  []
+  (let [seconds-since (r/atom 0)]
+    (fn []
+      (js/setTimeout #(swap! seconds-since inc) 1000)
+      [:div "Seconds since starting: " @seconds-since])))
 
 (defn test1 []
-  )
+  [timer])
