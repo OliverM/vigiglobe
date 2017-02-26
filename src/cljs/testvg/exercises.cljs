b356cb4234506da5f25efa6121932f706169778c
Oliver Mooney <oliver.mooney@gmail.com>
2017-02-26 13:08:55 +0000

    Test3 implementation

--- src/cljs/testvg/exercises.cljs
+++ src/cljs/testvg/exercises.cljs
@@ -15,25 +15,47 @@
    [timer]
    [:div [:a {:href "/"} "Go back to the home page."]]])
 
-(def shared-timer
-  "A namespace-level atom shared between multiple components. Should really be
-  managed by Component or Mount or Integrant or a similar state management
-  library, but implemented as a bare atom for the purposes of this exercise."
-  (r/atom 0))
-
-;; start the shared timer
-(js/setInterval #(swap! shared-timer inc) 1000)
-
-(defn shared-timer-view
-  "Return a component displaying the current shared-timer value."
+(defn counter-gen
+  "Instantiate a counter that can be shared among any number of views."
   []
-  [:div "Seconds since starting: " @shared-timer])
+  (let [seconds-since (r/atom 0)
+        _ (js/setInterval #(swap! seconds-since inc) 1000)]
+    seconds-since))
+
+(defn counter-view
+  "Given a seconds counter, return a view on that counter."
+  [counter]
+  [:div "Seconds since starting: " @counter])
 
 (defn test2 []
-  [:div
-   [shared-timer-view]
-   [shared-timer-view]])
+  (let [counter (counter-gen)]
+    [:div
+     [ counter-view counter ]
+     [ counter-view counter ]
+     [:div [:a {:href "/"} "Go back to the home page."]]]))
 
-(defn test3 [])
+(defn pausable-counter-gen
+  []
+  (let [seconds-since (r/atom 0)
+        update (r/atom true)
+        _ (js/setInterval #(when @update
+                             (swap! seconds-since inc))
+                          1000)]
+    [seconds-since update]))
+
+(defn pausable-counter-view
+  "Given a pair of atoms containing a seconds counter and an update flag, return
+  a view that displays the counter value and a control pausing that value."
+  [[counter update]]
+  [:div
+   "Seconds since starting: " @counter
+   [:button {:on-click #(swap! update not)}
+    (if @update "Pause" "Resume")]])
+
+(defn test3 []
+  (let [pausable-counter (pausable-counter-gen)]
+    [:div
+     [pausable-counter-view pausable-counter]
+     [:div [:a {:href "/"} "Go back to the home page."]]]))
 
 (defn bonus [])
