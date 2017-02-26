dcc6c9e8b0a11b662cdbdfe8c0fc1761bf1e7c41
Oliver Mooney <oliver.mooney@gmail.com>
2017-02-26 12:32:59 +0000

    Added shared timer infrastructure

--- src/cljs/testvg/exercises.cljs
+++ src/cljs/testvg/exercises.cljs
@@ -15,8 +15,24 @@
    [timer]
    [:div [:a {:href "/"} "Go back to the home page."]]])
 
+(def shared-timer
+  "A namespace-level atom shared between multiple components. Should really be
+  managed by Component or Mount or Integrant or a similar state management
+  library, but implemented as a bare atom for the purposes of this exercise."
+  (r/atom 0))
+
+;; start the shared timer
+(js/setInterval #(swap! shared-timer inc) 1000)
+
+(defn shared-timer-view
+  "Return a component displaying the current shared-timer value."
+  []
+  [:div "Seconds since starting: " @shared-timer])
+
 (defn test2 []
-  [timer])
+  [:div
+   [shared-timer-view]
+   [shared-timer-view]])
 
 (defn test3 [])
 
