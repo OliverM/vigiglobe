68162e5548577bdb95b70560d696b05b7194e50e
Oliver Mooney <oliver.mooney@gmail.com>
2017-03-08 23:26:09 +0000

    Line path data generating

--- src/cljs/testvg/vigiglobe.cljs
+++ src/cljs/testvg/vigiglobe.cljs
@@ -17,6 +17,32 @@
 (def chart-dim {:width 500
                 :height 400})
 
+(defn line-chart
+  "Generate a linechart using the supplied data."
+  [data]
+  (let [data (map (fn [[timestamp value]]
+                    [(.parse js/Date timestamp) value])
+                  data)
+        time-start (-> data first first)
+        time-end (-> data last first)
+        xscale (-> (.scaleTime js/d3)
+                   (.domain (array time-start time-end))
+                   (.range (array 0 (:width chart-dim))))
+        v-extent (.extent js/d3 (clj->js (map second data)))
+        yscale (-> (.scaleLinear js/d3)
+                   (.domain v-extent)
+                   (.range (array (:height chart-dim) 0)))
+        line (-> (.line js/d3)
+                 (.x (fn [[timestamp _] _ _] (xscale timestamp)))
+                 (.y (fn [[_ value] _ _] (yscale value))))]
+    (.log js/console (line (clj->js data)))
+    ))
+
+(defn chart []
+  [:div
+   [:h3 "The test chart"]
+   [:div [:a {:href "/"} "Go back to the home page."]]])
+
 (defn default-handler [response]
   "Generic successful API response handler."
   (.log js/console (str response)))
@@ -27,20 +53,7 @@
 
 (defn linechart-data-received
   [response]
-  (let [data (map (fn [[timestamp value]]
-                    [(.parse js/Date timestamp) value])
-                  (get-in response [:data "messages"]))
-        time-start (-> data first first)
-        time-end (-> data last first)
-        xscale (-> (.scaleTime js/d3)
-                   (.domain (array time-start time-end))
-                   (.range (array 0 (:width chart-dim))))
-        v-extent (.extent js/d3 (clj->js (map second data)))
-        yscale (-> (.scaleLinear js/d3)
-                   (.domain v-extent)
-                   (.range (array (:height chart-dim) 0)))]
-    (.log js/console (xscale time-end))
-    ))
+  (line-chart (get-in response [:data "messages"])))
 
 (defn last-hour-timestamp
   "Generate an ISO timestamp one hour before the current system time."
@@ -59,9 +72,3 @@
                  :response-format :transit
                  :handler linechart-data-received
                  :error-handler default-error-handler}))
-
-
-(defn chart []
-  [:div
-   [:h3 "The test chart"]
-   [:div [:a {:href "/"} "Go back to the home page."]]])
