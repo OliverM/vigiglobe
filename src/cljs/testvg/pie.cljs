e6c412b1efaa8afca69ddf9450097b1804c90b72
Oliver Mooney <oliver.mooney@gmail.com>
2017-05-17 19:20:09 +0100

    Added captions and initial caption rendering

--- src/cljs/testvg/pie.cljs
+++ src/cljs/testvg/pie.cljs
@@ -6,31 +6,31 @@
 
 
 (def pie-data
-  (r/atom [{:value 19168970 :caption "!"}
-           {:value 12420480 :caption "!"}
-           {:value 7351108 :caption "!"}
-           {:value 5711486 :caption "!"}
-           {:value 3212437 :caption "!"}
-           {:value 1795212 :caption "!"}
-           {:value 317900 :caption "!"}
-           {:value 268802 :caption "!"}
-           {:value 722469 :caption "!"}
-           {:value 68661 :caption "!"}
-           {:value 2143790 :caption "!"}
-           {:value 1870559 :caption "!"}
-           {:value 6534 :caption "!"}
-           {:value 6838974 :caption "!"}
-           {:value 2546000 :caption "!"}
-           {:value 43985910 :caption "!"}
-           {:value 3021219.25 :caption "!"}
-           {:value 3021219.25 :caption "!"}
-           {:value 3021219.25 :caption "!"}
-           {:value 3021219.25 :caption "!"}
-           {:value 1370000 :caption "!"}
-           {:value 770000 :caption "!"}
-           {:value 371465 :caption "!"}
-           {:value 2022828 :caption "!"}
-           {:value 711610 :caption "!"}
+  (r/atom [{:value 19168970 :caption "Income Tax"}
+           {:value 12420480 :caption "VAT"}
+           {:value 7351108 :caption "Corporation Tax"}
+           {:value 5711486 :caption "Excise Duty"}
+           {:value 3212437 :caption "Other tax revenue"}
+           {:value 1795212 :caption "Central Bank surplus"}
+           {:value 317900 :caption "Receipts from local government fund"}
+           {:value 268802 :caption "Dividends"}
+           {:value 722469 :caption "Other non-tax current revenue"}
+           {:value 68661 :caption "EU receipts"}
+           {:value 2143790 :caption "Loan repayment"}
+           {:value 1870559 :caption "Banking stabilisation measure"}
+           {:value 6534 :caption "Other capital receipts"}
+           {:value 6838974 :caption "Servicing of national debt"}
+           {:value 2546000 :caption "Net borrowing/(repayment) of debt"}
+           {:value 43985910 :caption "Voted expenditure"}
+           {:value 3021219.25 :caption "Social Insurance Fund (SIF)"}
+           {:value 3021219.25 :caption "Local Government Fund (LGF)"}
+           {:value 3021219.25 :caption "European Agricultural Guidance and Guarantee Fund (EAGGF)"}
+           {:value 3021219.25 :caption "Other Departmental expenditure and Departmental funds"}
+           {:value 1370000 :caption "Loan to Social Insurance Fund"}
+           {:value 770000 :caption "EAGGF guarantee agriculture"}
+           {:value 371465 :caption "Other capital expenditure"}
+           {:value 2022828 :caption "Contribution to EU budget"}
+           {:value 711610 :caption "Other non-voted current expenditure"}
            ]))
 
 (def chart-dim {:width 500 :height 500 :margin 30})
@@ -42,19 +42,24 @@
         radius (/ (:width chart-dim) 2)
         full-width (+ (* 2 margin) (:width chart-dim))
         full-height (+ (* 2 margin) (:height chart-dim))
+        data @pie-data
         arcs ((-> (.pie js/d3)
                   (.sort nil)
                   (.value #(aget % "value")))
-              (clj->js @pie-data))
+              (clj->js data))
         arcfn (-> (.arc js/d3)
                   (.outerRadius radius)
                   (.innerRadius 100))
         ]
     [:svg {:viewBox (str "0 0 " full-width " " full-height)
            :width full-width}
-     [:g {:transform (str "translate(" (/ full-height 2) "," (/ full-height 2) ")")}
+     [:g 
       (map-indexed
        (fn [index segment]
          ^{:key index}
-         [:path {:fill "green" :stroke "white" :d (arcfn segment) }])
+         [:g {:transform (str "translate(" (/ full-height 2) "," (/ full-height 2) ")")}
+          [:path {:fill  "green" :stroke "white" :d (arcfn segment)}]
+          (let [coords (.centroid arcfn segment)] 
+            [:text {:fill "blue" :x (aget coords 0) :y (aget coords 1)}
+             (:caption (nth data index))])])
        arcs)]]))
